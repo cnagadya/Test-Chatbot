eadf91b603c55be3a63e2d3cead0c045e24bcbc3
Christine R. Nagadya <christine@Christines-MacBook-Pro.local>
2019-06-30 12:03:59 +0200

    Testing model using kampala

--- nlu_model.py
+++ nlu_model.py
@@ -1,6 +1,6 @@
 from rasa_nlu.converters import load_data
 from rasa_nlu.config import RasaNLUConfig  # load config file
-from rasa_nlu.model import Trainer
+from rasa_nlu.model import Trainer, Metadata, Interpreter
 
 
 def train_nlu(data, config, model_dir):
@@ -11,5 +11,11 @@ def train_nlu(data, config, model_dir):
     model_directory = trainer.persist(model_dir, fixed_model_name="nlu")
 
 
+def run_nlu():
+    interpreter = Interpreter.load("models/nlu/default/nlu", RasaNLUConfig("config.json"))
+    print(print(interpreter.parse("Whats the weather in Kampala")))
+
+
 if __name__ == "__main__":
-    train_nlu("./data/data.json", "config.json", "./models/nlu")
+    # train_nlu("./data/data.json", "config.json", "./models/nlu")
+    run_nlu()
